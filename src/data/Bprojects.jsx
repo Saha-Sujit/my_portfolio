1c845227724f141cbf9c04cbaa1f8952435bc4de
Sujit Saha <sahasujit14@gmail.com>
2022-06-30 19:22:40 +0530

    react second project

--- src/data/Bprojects.jsx
+++ src/data/Bprojects.jsx
@@ -1,11 +1,21 @@
 const bprojects = [
   {
+    id: 1,
     name: "React News App",
     description:
       "News App/Website using React JS, Tailwind CSS, and NewsApi.org",
     image: require("../assets/news_app.png"),
     github: "https://github.com/Saha-Sujit/react_news",
     website: "",
+  },
+  {
+    id: 2,
+    name: "MERN Product Catalog",
+    description:
+      "Product Catalog made using MERN (React Js, Node Js, Express Js, and MongoDB). Product Catalog is website where users can register and list, edit, and delete their products.",
+    image: require("../assets/product_catelog.png"),
+    github: "https://github.com/Saha-Sujit/mern-product-catalog",
+    website: "https://product-catelog.netlify.app/",
   },
 ];
 
